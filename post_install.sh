1958df2dbc2e0862a6f3c632f68e256e32425be7
Cesar Zermeno <you@example.com>
2025-10-09 16:19:11 -0700

    fff

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Install the necessary browser executable and its system dependencies
+# 1. Set the installation directory to be within the Streamlit app's environment
+# The location $PWD/browser_cache is always accessible inside the Streamlit container.
+export PLAYWRIGHT_BROWSERS_PATH=$PWD/browser_cache
+
+# 2. Run the install command with system dependencies
 python -m playwright install --with-deps chromium
